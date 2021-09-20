6fab44413d99729d368614164f471754db464d7e
Robert Kawiak <robert.m.kawiak@intel.com>
2021-09-20 14:35:06 -0700

    updated API to GPA Framework 21.2 version

--- GPAPlugin/Source/ThirdParty/GPALibrary/include/igpa-shim-loader.h
+++ GPAPlugin/Source/ThirdParty/GPALibrary/include/igpa-shim-loader.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-        Copyright 2020 - 2021 Intel Corporation.
+© Intel Corporation.
  
 This software and the related documents are Intel copyrighted materials,
 and your use of them is governed by the express license under which they
@@ -17,6 +17,7 @@ License.
 
 #pragma once
 #include "igpa-config.h"
+#include "utility/common.h"
 #include <memory>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -35,6 +36,24 @@ struct IGPA
         Failed         //!< Operation failed
     };
 
+    //! @brief Selects APIs that will be hooked by the GPA shim
+    //! @param mask Bitmask containing values from gpa::utility::HookApiFlagBits
+    //! @detail This method needs to be called before Initialize to have effect
+    //!         All APIs are hooked by default
+    //!         It is recommended to have kHookWin32 flag always enabled, unless
+    //!         there is specific reason not to
+    virtual void SetHookApiMask(gpa::utility::HookApiFlags mask) = 0;
+
+    //! @brief Enables layer with provided name
+    //! @detail This method needs to be called before Initialize to have effect
+    virtual void AddLayer(char const* layerName) = 0;
+
+    //! @brief Adds a key-value pair parameter to initialized layer
+    //! @detail This method needs to be called before Initialize to have effect
+    //!         The layer must be added via AddLayer prior to this call
+    //!         Available layer parameters can be inspected via gpa-help tool
+    virtual void AddLayerParameter(char const* layerName, char const* parameterKey, char const* parameterValue) = 0;
+
     //! @brief  Initialize the GPA capture library
     //! @return possible errors:
     //!         NotSupported if the method is not supported for current API or platform
@@ -51,3 +70,6 @@ struct IGPA
 //! @brief     Object factory method returning a created GPA instance
 //! @param[in] Path to directory with installed GPA binaries
 GPA_EXPORT IGPA* GetGPAInterface(const std::string& gpaBinaryPath);
+
+//! @brief GetGPAInterface typedef
+typedef IGPA* (*PFN_GetGPAInterface)(const std::string&);
